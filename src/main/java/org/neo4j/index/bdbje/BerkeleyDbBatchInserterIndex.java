c7defb58c061aac2a513b54a9389debe62123172
Peter Neubauer <peter@neubauer.se>
2011-02-06 01:53:12 +0100

    First working test. No support for TX, and memory configuration problems in tests, but basic operations start to work.

---
+++ src/main/java/org/neo4j/index/bdbje/BerkeleyDbBatchInserterIndex.java
@@ -0,0 +1,196 @@
+/**
+ * Copyright (c) 2002-2011 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.index.bdbje;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.neo4j.graphdb.index.BatchInserterIndex;
+import org.neo4j.graphdb.index.IndexHits;
+import org.neo4j.helpers.Pair;
+import org.neo4j.kernel.impl.batchinsert.BatchInserter;
+import org.neo4j.kernel.impl.batchinsert.BatchInserterImpl;
+import org.neo4j.kernel.impl.cache.LruCache;
+
+import com.sleepycat.je.Database;
+import com.sleepycat.je.DatabaseConfig;
+import com.sleepycat.je.DatabaseEntry;
+import com.sleepycat.je.Environment;
+import com.sleepycat.je.EnvironmentConfig;
+import com.sleepycat.je.LockMode;
+import com.sleepycat.je.OperationStatus;
+
+class BerkeleyDbBatchInserterIndex implements BatchInserterIndex
+{
+    private final IndexIdentifier identifier;
+    
+    private final boolean createdNow;
+    private Map<String, LruCache<String, Collection<Long>>> cache;
+
+    private Map<String, Database> dbs = new HashMap<String, Database>();
+
+    private Pair<String, Boolean> storeDir;
+
+    BerkeleyDbBatchInserterIndex( BerkeleyDbBatchInserterIndexProvider provider,
+            BatchInserter inserter, IndexIdentifier identifier, Map<String, String> config )
+    {
+        String dbStoreDir = ((BatchInserterImpl) inserter).getStore();
+        storeDir = BerkeleyDbDataSource.getStoreDir( dbStoreDir + "/"+identifier.indexName );
+        this.createdNow = storeDir.other();
+        this.identifier = identifier;
+    }
+
+    public void add( long entityId, Map<String, Object> properties )
+    {
+        try
+        {
+            for ( Map.Entry<String, Object> entry : properties.entrySet() )
+            {
+                String key = entry.getKey();
+                Database db = dbs.get(key);
+                if(null == db ) {
+                    db = createDB(key);
+                    dbs.put( key, db );
+                }
+                String value = entry.getValue().toString();
+                DatabaseEntry valueEntry = new DatabaseEntry(value.getBytes("UTF-8"));
+                ByteArrayOutputStream baus = new ByteArrayOutputStream();
+                DataOutputStream dos = new DataOutputStream( baus );
+                dos.writeLong( entityId );
+                dos.flush();
+                db.put( null, valueEntry, new DatabaseEntry(baus.toByteArray()) );
+                dos.close();
+            }
+        }
+        catch ( Exception e )
+        {
+            throw new RuntimeException( e );
+        }
+    }
+    
+    private Database createDB( String key )
+    {
+        try
+        {
+            EnvironmentConfig environmentConfig = new EnvironmentConfig();
+            environmentConfig.setAllowCreate( true );
+            environmentConfig.setConfigParam( "java.util.logging.level", "INFO" );
+            // perform other environment configurations
+            String dir = BerkeleyDbDataSource.getStoreDir( this.storeDir.first() + "/" + key ).first();
+            Environment environment = new Environment( new File(
+                    dir ), environmentConfig );
+            environmentConfig.setTransactional( false );
+            DatabaseConfig databaseConfig = new DatabaseConfig();
+            databaseConfig.setAllowCreate( true );
+            // perform other database configurations
+            Database db = environment.openDatabase( null, key,
+                    databaseConfig );
+            return db;
+        }
+        catch ( Exception e )
+        {
+            throw new RuntimeException( e );
+        }
+    }
+
+
+    
+    public void updateOrAdd( long entityId, Map<String, Object> properties )
+    {
+        try
+        {
+            add( entityId, properties );
+        }
+        catch ( Exception e )
+        {
+            throw new RuntimeException( e );
+        }
+    }
+
+
+
+    public IndexHits<Long> get( String key, Object value )
+    {
+        ArrayList<Long> resultList = new ArrayList<Long>();
+        Database db = dbs.get( key );
+        if(null == db) {
+            return new IndexHitsImpl<Long>( resultList, 0 );
+        }
+        DatabaseEntry result = new DatabaseEntry();
+        try
+        {
+            OperationStatus status = db.get( null, new DatabaseEntry(value.toString().getBytes("UTF-8")), result, LockMode.READ_UNCOMMITTED );
+            if(status == OperationStatus.NOTFOUND) {
+                return new IndexHitsImpl<Long>( resultList, 0 );
+            }
+            byte[] data = result.getData();
+            if(null==data) {
+                return new IndexHitsImpl<Long>( resultList, 0 );
+            }
+            DataInputStream dis = new DataInputStream( new ByteArrayInputStream( data ) );
+            resultList.add( dis.readLong() );
+        }
+        catch ( Exception e )
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return new IndexHitsImpl<Long>( resultList, resultList.size() );
+    }
+
+
+    public void shutdown()
+    {
+    }
+    
+    public void flush()
+    {
+        //writerModified = true;
+    }
+
+    @Override
+    public IndexHits<Long> query( String key, Object queryOrQueryObject )
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public IndexHits<Long> query( Object queryOrQueryObject )
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setCacheCapacity( String key, int size )
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
---
+++ src/test/java/org/neo4j/index/bdbje/TestBerkeleyBatchInsert.java
@@ -0,0 +1,193 @@
+/**
+ * Copyright (c) 2002-2011 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.index.bdbje;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.index.BatchInserterIndex;
+import org.neo4j.graphdb.index.BatchInserterIndexProvider;
+import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexManager;
+import org.neo4j.helpers.collection.IteratorUtil;
+import org.neo4j.helpers.collection.MapUtil;
+import org.neo4j.kernel.EmbeddedGraphDatabase;
+import org.neo4j.kernel.impl.batchinsert.BatchInserter;
+import org.neo4j.kernel.impl.batchinsert.BatchInserterImpl;
+
+public class TestBerkeleyBatchInsert extends Neo4jTestCase
+{
+    private static final String PATH = "target/var/batch";
+
+    @Before
+    public void cleanDirectory()
+    {
+        Neo4jTestCase.deleteFileOrDirectory( new File( PATH ) );
+    }
+
+    @Test
+    public void testSome() throws Exception
+    {
+        BatchInserter inserter = new BatchInserterImpl( PATH );
+        BatchInserterIndexProvider provider = new BerkeleyDbBatchInserterIndexProvider(
+                inserter );
+        BatchInserterIndex index = provider.nodeIndex( "users",
+                BerkeleyDbIndexProvider.DEFAULT_CONFIG );
+        Map<Integer, Long> ids = new HashMap<Integer, Long>();
+        for ( int i = 0; i < 100; i++ )
+        {
+            long id = inserter.createNode( null );
+            index.add( id, MapUtil.map( "name", "Joe" + i, "other", "Schmoe" ) );
+            ids.put( i, id );
+        }
+
+        for ( int i = 0; i < 100; i++ )
+        {
+            assertContains( index.get( "name", "Joe" + i ), ids.get( i ) );
+        }
+        provider.shutdown();
+        inserter.shutdown();
+
+        GraphDatabaseService db = new EmbeddedGraphDatabase( PATH );
+        assertTrue( db.index().existsForNodes( "users" ) );
+        Index<Node> dbIndex = db.index().forNodes( "users" );
+        for ( int i = 0; i < 100; i++ )
+        {
+            assertContains( dbIndex.get( "name", "Joe" + i ),
+                    db.getNodeById( ids.get( i ) ) );
+        }
+
+        Collection<Node> nodes = new ArrayList<Node>();
+        for ( long id : ids.values() )
+        {
+            nodes.add( db.getNodeById( id ) );
+        }
+        db.shutdown();
+    }
+
+   
+    @Ignore
+    @Test
+    public void testInsertionSpeed()
+    {
+        BatchInserter inserter = new BatchInserterImpl( PATH );
+        BatchInserterIndexProvider provider = new BerkeleyDbBatchInserterIndexProvider( inserter );
+        BatchInserterIndex index = provider.nodeIndex( "yeah", BerkeleyDbIndexProvider.DEFAULT_CONFIG );
+        index.setCacheCapacity( "key", 1000000 );
+        long t = System.currentTimeMillis();
+        for ( int i = 0; i < 1000000; i++ )
+        {
+            Map<String, Object> properties = MapUtil.map( "key", "value" + i );
+            long id = inserter.createNode( properties );
+            index.add( id, properties );
+        }
+        System.out.println( "insert:" + ( System.currentTimeMillis() - t ) );
+        index.flush();
+
+        t = System.currentTimeMillis();
+        for ( int i = 0; i < 1000000; i++ )
+        {
+            IteratorUtil.count( (Iterator<Long>) index.get( "key", "value" + i ) );
+        }
+        System.out.println( "get:" + ( System.currentTimeMillis() - t ) );
+    }
+
+    @Test
+    public void testFindCreatedIndex()
+    {
+        String indexName = "persons";
+        BatchInserter inserter = new BatchInserterImpl( PATH );
+        BerkeleyDbBatchInserterIndexProvider indexProvider = new BerkeleyDbBatchInserterIndexProvider(
+                inserter );
+        BatchInserterIndex persons = indexProvider.nodeIndex( "persons",BerkeleyDbIndexProvider.DEFAULT_CONFIG );
+        Map<String, Object> properties = MapUtil.map( "name", "test" );
+        long node = inserter.createNode( properties );
+        persons.add( node, properties );
+        indexProvider.shutdown();
+        inserter.shutdown();
+        GraphDatabaseService graphDb = new EmbeddedGraphDatabase( PATH );
+        Transaction tx = graphDb.beginTx();
+        try
+        {
+            IndexManager indexManager = graphDb.index();
+            Assert.assertFalse( indexManager.existsForRelationships( indexName ) );
+            Assert.assertTrue( indexManager.existsForNodes( indexName ) );
+            Assert.assertNotNull( indexManager.forNodes( indexName ) );
+            Index<Node> nodes = graphDb.index().forNodes( indexName );
+            Assert.assertTrue( nodes.get( "name", "test" ).hasNext() );
+            tx.success();
+            tx.finish();
+        }
+        finally
+        {
+            graphDb.shutdown();
+        }
+    }
+
+    @Ignore
+    @Test
+    public void testCanIndexRelationships()
+    {
+        BatchInserter inserter = new BatchInserterImpl( PATH );
+        BatchInserterIndexProvider indexProvider = new BerkeleyDbBatchInserterIndexProvider(
+                inserter );
+        BatchInserterIndex edgesIndex = indexProvider.relationshipIndex(
+                "edgeIndex",
+                MapUtil.stringMap( "provider", "lucene", "type", "exact" ) );
+
+        long nodeId1 = inserter.createNode( MapUtil.map( "ID", "1" ) );
+        long nodeId2 = inserter.createNode( MapUtil.map( "ID", "2" ) );
+        long relationshipId = inserter.createRelationship( nodeId1, nodeId2,
+                EdgeType.KNOWS, null );
+
+        edgesIndex.add( relationshipId,
+                MapUtil.map( "EDGE_TYPE", EdgeType.KNOWS.name() ) );
+        edgesIndex.flush();
+
+        assertEquals(
+                String.format( "Should return relationship id" ),
+                new Long( relationshipId ),
+                edgesIndex.query( "EDGE_TYPE", EdgeType.KNOWS.name() ).getSingle() );
+
+        indexProvider.shutdown();
+        inserter.shutdown();
+    }
+    
+    private enum EdgeType implements RelationshipType
+    {
+        KNOWS
+    }
+}
